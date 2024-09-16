297cade46409ac8bc01869edbb1370aeeedbf061
Tanish Saini <tanish.saini@deeporion.com>
2024-09-16 12:55:55 +0530

    remove comments

--- src/Componen/Profile.tsx
+++ src/Componen/Profile.tsx
@@ -14,21 +14,6 @@ import {
 import { Navigate } from 'react-router-dom';
 import { FormValues, ProfileProps, ProfileState } from './types';
 import { profileSchema } from './validationSchemas';
-// export { validationSchema } from './validationSchemas'
-
-
-// const validationSchema = Yup.object({
-//     name: Yup.string().required('Enter your name').min(3, 'Name must be at least 3 characters'),
-//     number: Yup.string()
-//         .required('Enter your mobile number')
-//         .matches(/^[0-9]+$/, 'Must be only numbers')
-//         .min(10, 'Must be exactly 10 digits')
-//         .max(10, 'Must be exactly 10 digits'),
-//     position: Yup.string().required('Select your position'),
-//     gender: Yup.string().required('Select your gender'),
-//     address: Yup.string().required('Enter your address'),
-//     agree: Yup.boolean().oneOf([true], 'You must agree to the terms').required('Required'),
-// });
 
 class Profile extends Component<ProfileProps, ProfileState> {
     state: ProfileState = {
